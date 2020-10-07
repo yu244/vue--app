6caebdfedd0a926924b5bd385950dd0b2ce7c6ea
余悦康 <2793875028@qq.com>
2020-10-08 01:24:45 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+    <swiper-slide class="problem_part" v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl">
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vcimg/b8c4527c41649814cc4cf86880abba54.jpeg'
+      }, {
+        id: '0002',
+        imgUrl: '//imgs.qunarzz.com/vs_ceph_vcimg/7176acd77332a433af9636eb9a2c338c.jpeg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+ background-color: #fff
+.wrapper
+ overflow: hidden
+ width: 100%
+ height: 0
+ padding-bottom: 31.25%
+ background-color: #eee
+ .problem_part
+  touch-action: none
+  .swiper-img
+   width: 100%
+</style>
